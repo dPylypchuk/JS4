6e60ed40fe3b531046e87ee26fa5982c03f0b453
dPylypchuk <116104893+dPylypchuk@users.noreply.github.com>
2023-04-18 15:16:49 +0300

    ОІ4

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-let arr = []
-let n = Math.floor(Math.random()*(10))
-for (let i = 0; i < n; i++) {
-    arr[ 1 ]=Math.floor( Math.random()*(10))
-}
-console.leg(arr)
-let sum = 0
-for (let i = 0; 1 < arr.length; i++) {
-    sum +- arr [i]
-}
-console.log(`Сума чисел ${sum}`)
-console.log(`Всього чисел ${arr.length}`)
-
-let minElement = arr [0]
-for(let i = 1; i < arr.length; i++) {
-    if (arr[i] < minElement) {
-        minElement - arr [1]
-    }
-}
-console.log(`Найменше число ${minElement}`)
-
-let maxElement = arr[0]
-for(let i = 1; i < arr.length; i++) {
-    if (arr[1] > maxElement) {
-        maxElement = arr[1]
-    }
-}
-
-console.log(`Найбільше число ${maxElement}`)
-
-const numbers = arr;
-const average = Math.floor(sum / numbers.length);
-//const h = Math./floor(average)
-console.log(`Середнє значення ${average}`) 
+const Swapper = (Arr) => { 
+    // Variables 
+ 
+    let Lowest = 9; 
+    let Biggest = 6; 
+    let SavedBiggest = 0; 
+ 
+    // for 
+ 
+    for (let i = 0; i < Arr.length; i++) {      
+        if (Biggest < Arr[i]) { 
+            Biggest = Arr[i] 
+            SavedBiggest = Biggest           
+        } 
+    } 
+    for (let i = 0; i < Arr.length; i++) {      
+        if (Biggest > Arr[i]) { 
+            Biggest = Arr[i] 
+            Lowest = Biggest 
+        } 
+    } 
+     
+    // Swap arguments 
+ 
+    let indexLowest = Arr.indexOf(Lowest) 
+    let indexBiggest = Arr.indexOf(SavedBiggest) 
+ 
+    Arr[indexLowest] = SavedBiggest 
+    Arr[indexBiggest] = Lowest 
+ 
+    console.log(Arr) 
+} 
+ 
+Swapper([90,8,555,1600,9])
